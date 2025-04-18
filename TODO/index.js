a20043b5dfbb2312e7cb966b8a8b1e25998da197
Parishkrat <parish.mishra2001@gmail.com>
2025-04-18 20:56:00 +0530

    Update index.js

--- TODO/index.js
+++ TODO/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const todo = require('./Routes/todo.js');
-const bcrypt = require('bcrypt');
 const userRoutes = require('./Routes/userRoutes.js');
-const jwt = require('jsonwebtoken');
 const app = express();
 
 dotenv.config();
