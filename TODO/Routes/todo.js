767dfabdd29281def6daca88c81c2d7ea6a234a4
Parishkrat <parish.mishra2001@gmail.com>
2025-04-18 19:15:43 +0530

    Update todo.js

--- TODO/Routes/todo.js
+++ TODO/Routes/todo.js
@@ -1,74 +1,59 @@
-const router=require('express').Router();
-const Todo=require('../Models/todo')
-//list all todo
+const router = require('express').Router();
+const Todo = require('../Models/todo');
 
-router.get('/',(req,res)=>{
-Todo.find().exec((err,todos)=>{
-    if(err){
-        return res.json({error:err})
-    }
-    return res.json({data:todos})
-})
-})
+// List all todos
+router.get('/', (req, res) => {
+  Todo.find().exec((err, todos) => {
+    if (err) return res.json({ error: err });
+    return res.json({ data: todos });
+  });
+});
 
-//create todo
-router.post('/create',(req,res)=>{
-    const todo=Todo({
-        title:req.body.title,
-        content:req.body.content,
-    })
-    todo.save((err,todo)=>{
-        if(err){
-            return res.json({error:err})
-        }
-        return res.json({data:todos})
-    })
+// Create todo
+router.post('/create', (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+    content: req.body.content,
+  });
 
-})
-
-//edit todo
-router.put('/:id',(req,res)=>{
-
- Todo.findById(req.params.id)
-.exec((err, todo) => {
-  if (err) {
-    return res.json({ error: err });
-  }
-  todo.title = req.body.title ?? todo.title;
-  todo.content = req.body.content ?? todo.content;
-  todo.completed = req.body.completed ?? todo.completed;
   todo.save((err, todo) => {
-    if (err) {
-      return res.json({ error: err });
-    }
+    if (err) return res.json({ error: err });
     return res.json({ data: todo });
-  })
+  });
 });
+
+// Edit todo
+router.put('/:id', (req, res) => {
+  Todo.findById(req.params.id).exec((err, todo) => {
+    if (err) return res.json({ error: err });
+    if (!todo) return res.status(404).json({ error: 'Todo not found' });
+
+    todo.title = req.body.title ?? todo.title;
+    todo.content = req.body.content ?? todo.content;
+    todo.completed = req.body.completed ?? todo.completed;
+
+    todo.save((err, updatedTodo) => {
+      if (err) return res.json({ error: err });
+      return res.json({ data: updatedTodo });
+    });
+  });
 });
 
-//Delete
-
-router.delete('/',(req,res)=>{
-    Todo.remove({
-        _id:req.params.id
-    }.exec((err,todo)=>{
-        if(err){
-            return res.json({error:err})
-        }
-        if (result.deletedCount == 0) {
-            return res.json({ data: 'No Todo Found with given id' });
-          }
-          return res.json({ data: 'Deleted Successfully' });
-    }))
-
-})
+// Delete todo
+router.delete('/:id', (req, res) => {
+  Todo.deleteOne({ _id: req.params.id }).exec((err, result) => {
+    if (err) return res.json({ error: err });
+    if (result.deletedCount === 0) {
+      return res.json({ data: 'No Todo Found with given id' });
+    }
+    return res.json({ data: 'Deleted Successfully' });
+  });
+});
 
 // Creating a new Todo for a user
-
-
 const createTodo = async (req, res) => {
   const { title, description } = req.body;
-  const userId = req.user._id; // assuming auth middleware adds user to req
+  const userId = req.user._id; // assuming auth middleware adds user
 
   try {
     const todo = await Todo.create({ title, description, user: userId });
@@ -78,5 +63,4 @@ const createTodo = async (req, res) => {
   }
 };
 
-
-module.exports=router
+module.exports = router;
